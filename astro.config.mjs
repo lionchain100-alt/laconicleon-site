6265310560379c63878645d007e399c084094b8a
Leon Reed <lionchain100-alt@users.noreply.github.com>
2026-09-01 21:20:42 +0800

    fix: exclude admin from search discovery

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://laconicleon.com',
-  integrations: [sitemap()],
+  integrations: [sitemap({ filter: (page) => !new URL(page).pathname.startsWith('/admin') })],
 });
